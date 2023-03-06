a2e47a03cf89c6f1e33e07ea22160cae6b52a514
NeptuneRjo <rjo9000@outlook.com>
2023-03-06 03:03:20 -0500

    compile

--- root/backend/dist/global/scraper.js
+++ root/backend/dist/global/scraper.js
@@ -3,65 +3,54 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.scrape = void 0;
 const puppeteer_1 = __importDefault(require("puppeteer"));
 const fs_1 = require("fs");
-async function scrapeTopFifty() {
+const scrape = async () => {
+    try {
+        const browser = await puppeteer_1.default.launch();
+        const page = await browser.newPage();
+        await page.goto('https://finance.yahoo.com/most-active/?offset=0&count=50');
+        // Set screen size
+        await page.setViewport({ width: 1920, height: 1080 });
+        // Gets the 50 symbols and stores them as an array
+        const symbolsArray = await page.evaluate(() => Array.from(document.querySelectorAll('div#scr-res-table div table tbody tr td a'), (element) => element.textContent));
+        if (symbolsArray.length < 50) {
+            throw Error('The returned length of the symbols is less that 50, skipping JSON overwrite');
+        }
+        return symbolsArray;
+    }
+    catch (error) {
+        console.log('Scrape Error: ', error);
+        return undefined;
+    }
+};
+exports.scrape = scrape;
+async function updateSymbolsJson() {
     if ((0, fs_1.existsSync)('symbols.json')) {
         const rawJson = (0, fs_1.readFileSync)('symbols.json');
         const parsedJson = JSON.parse(rawJson.toString());
-        const { date, symbols } = parsedJson;
+        const { date } = parsedJson;
         const currentDate = new Date().toISOString().slice(0, 10);
         if (currentDate > date || date === undefined) {
-            ;
-            (async () => {
-                try {
-                    const browser = await puppeteer_1.default.launch();
-                    const page = await browser.newPage();
-                    await page.goto('https://finance.yahoo.com/most-active/?offset=0&count=50');
-                    // Set screen size
-                    await page.setViewport({ width: 1920, height: 1080 });
-                    // Gets the 50 symbols and stores them as an array
-                    const symbolsArray = await page.evaluate(() => Array.from(document.querySelectorAll('div#scr-res-table div table tbody tr td a'), (element) => element.textContent));
-                    if (symbolsArray.length < 50) {
-                        throw Error('The returned length of the symbols is less that 50, skipping JSON overwrite');
-                    }
-                    // Updates the values in the in-memory object (parsedJson)
-                    parsedJson.date = currentDate;
-                    parsedJson.symbols = symbolsArray;
-                    // Serialize as JSON and write it to the file
-                    (0, fs_1.writeFileSync)('symbols.json', JSON.stringify(parsedJson, null, 2)); // formating
-                }
-                catch (error) {
-                    console.log('Scrape Error: ', error);
-                }
-            })();
+            const symbolsArray = await (0, exports.scrape)();
+            parsedJson.date = currentDate;
+            // Redundancy; if the scraper runs into an error, leave the symbols as is
+            parsedJson.symbols =
+                symbolsArray === undefined ? parsedJson.symbols : symbolsArray;
+            // Serialize as JSON and write it to the file
+            (0, fs_1.writeFileSync)('symbols.json', JSON.stringify(parsedJson, null, 2)); // formating
         }
     }
     else {
         const currentDate = new Date().toISOString().slice(0, 10);
-        (async () => {
-            try {
-                const browser = await puppeteer_1.default.launch();
-                const page = await browser.newPage();
-                await page.goto('https://finance.yahoo.com/most-active/?offset=0&count=50');
-                // Set screen size
-                await page.setViewport({ width: 1920, height: 1080 });
-                // Gets the 50 symbols and stores them as an array
-                const symbolsArray = await page.evaluate(() => Array.from(document.querySelectorAll('div#scr-res-table div table tbody tr td a'), (element) => element.textContent));
-                if (symbolsArray.length < 50) {
-                    throw Error('The returned length of the symbols is less that 50, skipping JSON overwrite');
-                }
-                const json = {
-                    date: currentDate,
-                    symbols: symbolsArray,
-                };
-                // Serialize as JSON and write it to the file
-                (0, fs_1.writeFileSync)('symbols.json', JSON.stringify(json, null, 2)); // formating
-            }
-            catch (error) {
-                console.log('Scrape Error: ', error);
-            }
-        })();
+        const symbolsArray = await (0, exports.scrape)();
+        const json = {
+            date: currentDate,
+            symbols: symbolsArray === undefined ? [] : symbolsArray,
+        };
+        // Serialize as JSON and write it to the file
+        (0, fs_1.writeFileSync)('symbols.json', JSON.stringify(json, null, 2)); // formating
     }
 }
-exports.default = scrapeTopFifty;
+exports.default = updateSymbolsJson;
