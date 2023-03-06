82209c6072769de6a7b1d8d5a46d52aa0bb6e3f7
NeptuneRjo <rjo9000@outlook.com>
2023-03-06 02:05:08 -0500

    create scraper for the top 50 stocks

---
+++ root/backend/src/global/scraper.ts
@@ -0,0 +1,49 @@
+import puppeteer from 'puppeteer'
+import { readFileSync, writeFileSync } from 'fs'
+
+const rawJson = readFileSync('symbols.json')
+const parsedJson = JSON.parse(rawJson.toString())
+
+const { date, symbols } = parsedJson
+const currentDate = new Date().toISOString().slice(0, 10)
+
+if (currentDate > date || date === undefined) {
+	;(async () => {
+		try {
+			const browser = await puppeteer.launch()
+			const page = await browser.newPage()
+
+			await page.goto(
+				'https://finance.yahoo.com/most-active/?offset=0&count=50'
+			)
+
+			// Set screen size
+			await page.setViewport({ width: 1920, height: 1080 })
+
+			// Gets the 50 symbols and stores them as an array
+			const symbolsArray = await page.evaluate(() =>
+				Array.from(
+					document.querySelectorAll(
+						'div#scr-res-table div table tbody tr td a'
+					),
+					(element) => element.textContent
+				)
+			)
+
+			if (symbolsArray.length < 50) {
+				throw Error(
+					'The returned length of the symbols is less that 50, skipping JSON overwrite'
+				)
+			}
+
+			// Updates the values in the in-memory object (parsedJson)
+			parsedJson.date = currentDate
+			parsedJson.symbols = symbolsArray
+
+			// Serialize as JSON and write it to the file
+			writeFileSync('symbols.json', JSON.stringify(parsedJson, null, 2)) // formating
+		} catch (error) {
+			console.log('Scrape Error: ', error)
+		}
+	})()
+}
