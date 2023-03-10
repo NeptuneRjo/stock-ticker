e015dfce25d066efa8720f95b961fbe148c1d8aa
NeptuneRjo <rjo9000@outlook.com>
2023-03-10 16:19:33 -0500

    create about page

---
+++ root/frontend/pages/about/index.tsx
@@ -0,0 +1,44 @@
+const About = () => {
+	return (
+		<>
+			<div>
+				<h3>About Stock Ticker</h3>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit
+					suscipit harum dolorum tempora, animi esse velit, iusto debitis illum
+					labore quasi similique, doloribus cupiditate itaque eaque earum
+					exercitationem. Quos, laudantium. Nobis error dolorem expedita libero,
+					assumenda magnam officiis ad aut, odit, earum nam enim. Dicta nam
+					illum vel nisi tenetur quibusdam facilis. Exercitationem, alias
+					incidunt quasi quaerat sapiente dicta autem!
+				</p>
+			</div>
+			<div>
+				<h4>Our Mission</h4>
+				<p>
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus
+					doloremque praesentium saepe deserunt hic quod neque, dignissimos,
+					minus fugiat eos dolorum? Ea iusto doloremque dolorum vitae temporibus
+					laborum ipsam sed.
+				</p>
+			</div>
+			<div>
+				<h4>Something Else</h4>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque aliquam
+					inventore cum ad maxime totam magni iure, odit itaque. Magnam tenetur
+					cumque quibusdam illum nobis possimus dolorum voluptate, assumenda
+					non?
+				</p>
+				<p>
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam
+					dolores officiis iste, quaerat sunt sed cupiditate! Iste temporibus
+					consectetur repellendus veniam necessitatibus magnam mollitia voluptas
+					corrupti doloribus, iusto accusamus omnis!
+				</p>
+			</div>
+		</>
+	)
+}
+
+export default About
