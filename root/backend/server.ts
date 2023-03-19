b7f0cd4b02f82ac4da11647d9a6dfbf80ea51262
NeptuneRjo <rjo9000@outlook.com>
2023-03-19 01:44:10 -0400

    cors

--- root/backend/server.ts
+++ root/backend/server.ts
@@ -17,6 +17,7 @@ const content = cache.get('content')
 const io = new Server(server, {
 	cors: {
 		origin: ['http://localhost:3000', 'https://stock-ticker.onrender.com/'],
+		credentials: true,
 	},
 })
 
@@ -24,6 +25,7 @@ const io = new Server(server, {
 app.use(
 	cors({
 		origin: ['http://localhost:3000', 'https://stock-ticker.onrender.com/'],
+		credentials: true,
 	})
 )
 
