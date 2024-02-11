beb499fe29d65acdd450a8cae9b9402013b35b9f
NeptuneRjo <rjo9000@outlook.com>
2024-02-11 03:35:32 -0500

    Create initialization file for postgres and postgres content

--- services/scraper/src/data/initializePostgres.ts
+++ services/scraper/src/data/initializePostgres.ts
@@ -1,10 +1,27 @@
-import {Client} from 'pg'
-import { initializeStockTable } from './tables/stockTable'
+import { Client } from 'pg'
 
 const client = new Client()
 
 client.connect().then(() => {
-    initializeStockTable(client)
+    setupStockTable()
 })
+
+async function setupStockTable () {
+    const text = `
+        CREATE TABLE IF NOT EXISTS stocks (
+            stock_id SERIAL PRIMARY KEY, 
+            symbol VARCHAR(32) NOT NULL, 
+            name VARCHAR(64) NOT NULL, 
+            last_price NUMERIC NOT NULL, 
+            market_time VARCHAR(24), 
+            change NUMERIC NOT NULL, 
+            change_positive BOOLEAN NOT NULL, 
+            change_percent NUMERIC NOT NULL, 
+            volume VARCHAR(24) NOT NULL, 
+            market_cap VARCHAR(24) NOT NULL
+        )
+    `
+    await client.query(text)
+}
 
 export default client
