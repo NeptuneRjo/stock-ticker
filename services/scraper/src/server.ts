aac2231783fdb8f0d2099cb87ba76a3f7dcbfc0d
NeptuneRjo <rjo9000@outlook.com>
2024-02-14 18:51:10 -0500

    Add cron and web hook to socket service

--- services/scraper/src/server.ts
+++ services/scraper/src/server.ts
@@ -2,26 +2,19 @@ import { scrapeStocks } from './scraper/scraper'
 import { Stock } from './types'
 import { parseStockHtmlElements } from './scraper/htmlParsing'
 import { fillDatabaseWithStocks } from './data/addStocks'
-import client from './data/initializePostgres'
-import express, { Request, Response } from 'express'
+import cron from 'node-cron'
+import axios from 'axios'
 
-const app = express()
-
-const scrapeAndPopulateDb = async () => {
+// 1-minute intervals
+const scrapeTask = cron.schedule('* */1 * * *', async () => {
     const stockHtmlElements: string[] = await scrapeStocks()
     const stocks: Stock[] = await parseStockHtmlElements(stockHtmlElements)
 
     await fillDatabaseWithStocks(stocks)
-}
 
-app.get("/", async (req: Request, res: Response) => {
-    await scrapeAndPopulateDb()
-
-    res.send(200)
+    axios.get('http://websocket_container:8000/emitt-event')
+        .then(() => console.log("notified socket service"))
+        .catch((err) => console.log(err))
 })
 
-const port = process.env.PORT || 4000
-
-app.listen(port, () => {
-	console.log(`Server sucessfully started and listening on port ${port}`)
-})
+scrapeTask.start()
