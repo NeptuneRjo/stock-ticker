690cb48493f80079b4f62197d503ac86d271be47
NeptuneRjo <rjo9000@outlook.com>
2024-02-16 03:16:51 -0500

    Remove root folder

--- root/backend/dist/jest.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    verbose: true,
-    automock: true,
-};
-exports.default = config;
