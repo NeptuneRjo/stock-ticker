180334fb132ceb3f9ddbd6887469c85e9b5c7827
NeptuneRjo <rjo9000@outlook.com>
2024-02-16 01:17:05 -0500

    Implement sockets and display stocks

--- services/frontend/src/App.tsx
+++ services/frontend/src/App.tsx
@@ -1,23 +1,74 @@
 import React from 'react';
 import './App.css';
+import { Stock } from './types';
+import io, { Socket } from 'socket.io-client'
 
 function App() {
+  const endpoint = process.env.REACT_APP_WEBSOCKET_ENDPOINT
+  const socket: Socket = io(endpoint as string)
+  
+  const [stocks, setStocks] = React.useState<Stock[]>([])
+
+  React.useEffect(() => {
+    socket.connect()
+    
+    	// **:**:client/server denotes the recipient
+    socket.emit('initialize:content:server')
+
+    // Prevent uneccessary updating
+    if (stocks.length === 0) {
+      socket.on('initialize:content:client', (data: Stock[]) => {
+        setStocks(data)
+      })    
+    }
+
+    socket.on('update:content:client', (data: Stock[]) => {
+      setStocks(data)
+    })
+
+    return () => {
+      socket.disconnect()
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app">
+      <div>
+        <h1>Home</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Displays the prices of 100 of the current most active stocks
+          <br />
+          <span>
+            Stock prices are updated every minute.
+          </span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      {stocks.length === 0 ? (
+        <div className='loader'>Loading...</div>
+      ) : (
+        <table>
+          <tr>
+            <th>Symbol</th>
+            <th>Name</th>
+            <th>Last Price (USD)</th>
+            <th>Change</th>
+            <th>Percent Change</th>
+            <th>Volume</th>
+            <th>Market Cap</th>
+          </tr>
+          {stocks.map((stock: Stock, key: number) => (
+              <tr key={key}>
+                <td>{stock.symbol}</td>
+                <td>{stock.name}</td>
+                <td>{stock.last_price}</td>
+                <td>{stock.change}</td>
+                <td>{stock.change_percent}</td>
+                <td>{stock.volume}</td>
+                <td>{stock.market_cap}</td>
+              </tr>
+          ))}
+        </table>
+      )}
     </div>
   );
 }
