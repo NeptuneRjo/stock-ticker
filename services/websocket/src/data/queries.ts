67814c71a6b5a04b5056650d6e1f947b75cf4620
NeptuneRjo <rjo9000@outlook.com>
2024-02-27 20:53:20 -0500

    Add methods for updating/adding; cleanup

--- services/websocket/src/data/queries.ts
+++ services/websocket/src/data/queries.ts
@@ -1,41 +1,53 @@
 import { Stock } from '../types'
 import * as db from './db'
 
+export const updateStocksInDatabase = async (stocks: Stock[]) => {
+    const startTime = performance.now()
+    
+    try {
+        // Clear stock table
+        await db.query('TRUNCATE TABLE stocks RESTART IDENTITY CASCADE')
+        
+        stocks.forEach(async (stock) => {
+            // Add updated stocks
+            await addStock(stock)
+                            
+            // Delete any stock_history rows that dont have a counter part
+            // Or if they're over 24 hours old
+            const cleanStockHistory = `
+                DELETE FROM stock_history
+                WHERE stock_symbol NOT IN (SELECT symbol FROM stocks)
+                OR price_timestamp < 
+                CURRENT_TIMESTAMP - INTERVAL '24 hours'
+            `
+            await db.query(cleanStockHistory)
+            
+            // Add new rows to stock_history
+            await addStockHistory(stock.last_price, stock.symbol)
+        })
+        
+        const endTime = performance.now()
+        
+        console.log(`Update database time in seconds: ${Math.round((endTime - startTime) / 1000)}`)
+    } catch (error) {
+        console.log(error)   
+    }
+}
+
 export const addStocksToDatabase = async (stocks: Stock[]) => {
     const startTime = performance.now()
-
+    
     try {
-        await db.query('TRUNCATE TABLE stocks  RESTART IDENTITY CASCADE')
-
+        await db.query('TRUNCATE TABLE stocks RESTART IDENTITY CASCADE')
+        await db.query('TRUNCATE TABLE stock_history RESTART IDENTITY CASCADE')
+        
         stocks.forEach(async (stock) => {
-            const text = `
-                INSERT INTO stocks (
-                    symbol, 
-                    name, 
-                    last_price, 
-                    change, 
-                    change_percent, 
-                    volume, 
-                    market_cap
-                ) 
-                VALUES ($1, $2, $3, $4, $5, $6, $7)
-            `
-            
-            const values = [
-                stock.symbol, 
-                stock.name, 
-                stock.last_price, 
-                stock.change, 
-                stock.change_percent, 
-                stock.volume, 
-                stock.market_cap
-            ]
-
-            await db.query(text, values)
+            await addStock(stock)
+            await addStockHistory(stock.last_price, stock.symbol)
         })
-
+        
         const endTime = performance.now()
-
+        
         console.log(`Fill database time in seconds: ${Math.round((endTime - startTime) / 1000)}`)
     } catch (error) {
         console.error(error)
@@ -58,23 +70,60 @@ export const setupStockTable = async () => {
     `
     await db.query(text)
 }
-
+    
 export const setupStockHistoryTable = async () => {
     const text = `
         CREATE TABLE IF NOT EXISTS stock_history (
             stock_history_id SERIAL PRIMARY KEY,
             price NUMERIC NOT NULL,
-            price_timestamp TIMESTAMP NOT NULL,
-            stock_id INTEGER REFERENCES stocks (stock_id)
+            price_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            stock_symbol VARCHAR(32) NOT NULL
         )
     `
-
+        
     await db.query(text)
 }
-
+        
 export const getStocksFromDatabase = async (): Promise<any[]> => {
     const text = `SELECT * FROM stocks`
     const results = await db.query(text)
-
+    
     return results.rows
+}
+
+async function addStockHistory (last_price: number, symbol: string) {
+    const query = `
+        INSERT INTO stock_history (price, stock_symbol)
+        VALUES ($1, $2)
+    `
+    const values = [
+        last_price,
+        symbol
+    ]
+    await db.query(query, values)
+}
+
+async function addStock (stock: Stock) {
+    const query = `
+        INSERT INTO stocks (
+            symbol, 
+            name, 
+            last_price, 
+            change, 
+            change_percent, 
+            volume, 
+            market_cap
+        ) 
+        VALUES ($1, $2, $3, $4, $5, $6, $7)
+    `
+    const values = [
+        stock.symbol, 
+        stock.name, 
+        stock.last_price, 
+        stock.change, 
+        stock.change_percent, 
+        stock.volume, 
+        stock.market_cap
+    ]
+    await db.query(query, values)
 }
